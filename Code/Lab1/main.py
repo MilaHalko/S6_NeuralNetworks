10607ce5eb76d54cf142b5495e1f94e89fbb87b7
Mila Halko <milagalko1@gmail.com>
2023-06-07 02:05:36 +0300

    Lab1 params & +report

--- Code/Lab1/main.py
+++ Code/Lab1/main.py
@@ -6,10 +6,9 @@ y = np.array([0, 1, 1, 0])
 
 xor_model = tf.keras.models.Sequential()
 xor_model.add(tf.keras.layers.Dense(5, input_dim=2, activation='relu'))
-xor_model.add(tf.keras.layers.Dense(5, activation='relu'))
 xor_model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 xor_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-xor_model.fit(x, y, epochs=1000, verbose=0)
+xor_model.fit(x, y, epochs=5000, verbose=0)
 loss_and_accuracy_scores = xor_model.evaluate(x, y)
 
 print(xor_model.summary())
